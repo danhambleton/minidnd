961ded1782661f1100f05053a3a998e41c2d2941
Daniel Hambleton <daniel@meshconsultants.ca>
2021-03-06 21:47:06 -0500

    map works

--- src/index.js
+++ src/index.js
@@ -10,17 +10,48 @@
 
 // import { setupDragAndDrop } from "./DragAndDrop.js";
 
+import * as L from "leaflet"
+
 main();
 
 function main() {
   let activeArea = document.getElementById("activeArea");
 
-  let canvas = document.createElement("canvas");
-  activeArea.appendChild(canvas);
+  var w = 30000;
+  var h = 30000;
+  var mapMinZoom = 2;
+  var mapMaxZoom = 7;
+  var _map = L.map('mapid', {
+    maxZoom: mapMaxZoom,
+    minZoom: mapMinZoom,
+    crs: L.CRS.Simple,
+    zoomControl: false,
+    wheelPxPerZoomLevel: 250,
+    attributionControl: false,
+    detectRetina: true
+  });
 
+  var _mapBounds = new L.LatLngBounds(
+    _map.unproject([0, h], mapMaxZoom),
+    _map.unproject([w, 0], mapMaxZoom));
+  _map.setMaxBounds(_mapBounds);
 
-  // // Set up drag-and-drop for the active area
+  var _mapCenter = _map.unproject([w / 2, h / 2], mapMaxZoom);
+  _map.setView(_mapCenter, -3);
+
+  var _tileLayer = L.tileLayer(
+    'assets/iwd-tiles-sq/{z}/{x}/{y}.png', {
+    minZoom: mapMinZoom, maxZoom: mapMaxZoom,
+    bounds: _mapBounds,
+    continuousWorld: false,
+    noWrap: true,
+    tileSize: 256,
+    crs: L.CRS.Simple,
+    detectRetina: true
+  }).addTo(_map);
+
+  // Set up drag-and-drop for the active area
   // setupDragAndDrop(activeArea, objCallback);
 
-
+  _map.fitBounds(_mapBounds)
 }
