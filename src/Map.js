0ce1c6e2d9f367f2ef79eae857563170995bd513
Daniel Hambleton <daniel@meshconsultants.ca>
2021-04-14 15:13:32 -0400

    perp hex maps

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,7 @@
 import aws from "aws-sdk"
 import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GUI } from "three/examples/jsm/libs/dat.gui.module.js";
 import * as MapShaders from "./MapShaders.js";
 
 class Map {
@@ -9,6 +11,7 @@ class Map {
  * 
  * .
  * @param {*} s3
+
   */
     constructor(container) {
 
@@ -19,15 +22,21 @@ class Map {
             secretAccessKey: process.env.SPACES_SECRET_KEY,
         });
 
+        var screenDims = new THREE.Vector2(container.offsetWidth, container.offsetHeight);
+        var origin = new THREE.Vector2(0.5, 0.5);
+        var gridScale = 10.0;
+        var imageScale = 1.0;
+        var imageDims = new THREE.Vector2(1920, 1080);
+
         //init threejs
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera( 75, container.offsetWidth / container.offsetHeight, 0.1, 100 );
+        const camera = new THREE.PerspectiveCamera(75, screenDims.x / screenDims.y, 0.1, 100);
 
         console.log(container.offsetWidth);
 
         const renderer = new THREE.WebGLRenderer();
-        renderer.setSize( container.offsetWidth, container.offsetHeight );
-        container.appendChild( renderer.domElement );
+        renderer.setSize(screenDims.x, screenDims.y);
+        container.appendChild(renderer.domElement);
 
         // instantiate a loader
         const loader = new THREE.TextureLoader();
@@ -35,60 +44,129 @@ class Map {
         // load a resource
         loader.load(
             // resource URL
-            'https://danbleton.nyc3.digitaloceanspaces.com/circle-of-fire-and-grace/sunblight-keep.png',
+            //'https://danbleton.nyc3.digitaloceanspaces.com/circle-of-fire-and-grace/beach_map.jpeg',
+            'https://danbleton.nyc3.digitaloceanspaces.com/circle-of-fire-and-grace/lost_city.jpeg',
 
             // onLoad callback
-            function ( texture ) {
-                // // in this example we create the material when the texture is loaded
-                // const material = new THREE.MeshBasicMaterial( {
-                //     map: texture
-                // } );
+            function (texture) {
 
-                // const geometry = new THREE.BoxGeometry();
-                // const cube = new THREE.Mesh( geometry, material );
-                // scene.add( cube );
-        
-                // camera.position.z = 5;
+                imageDims = new THREE.Vector2(texture.image.width, texture.image.height);
+                var aspect = imageDims.y / imageDims.x;
 
-                var quad = new THREE.Mesh(
-                    new THREE.PlaneGeometry(2, 2),
-                    new THREE.ShaderMaterial({
-                      vertexShader: MapShaders.buildMapVertexShader(),
-                      fragmentShader: MapShaders.buildMapFragmentShader(),
-                      depthWrite: false,
-                      depthTest: false,
-                      uniforms: {
-                        baseMap: { type: "t", value: texture }
-                      },
+                var imgQuad = new THREE.Mesh(
+                    new THREE.PlaneGeometry(10.0, aspect * 10.0),
+                    new THREE.MeshBasicMaterial({
+                        map: texture
                     })
-                  );
-                  scene.add(quad);
+                );
+                scene.add(imgQuad);
+
+                var params = {
+                    p_grid_scale: 100.0,
+                    p_image_scale: 1.0,
+                    p_origin_x: 0.5,
+                    p_origin_y: 0.5,
+                    p_grid_rot_x: 0.0,
+                    p_grid_rot_y: 0.0,
+                    p_grid_rot_z: 0.0,
+                    p_grid_pos_y: 0.0,
+                }
+
+                var mapUniforms = {
+                    baseMap: { type: "t", value: texture }, //fog texture??
+                    u_grid_scale: { value: gridScale },
+                    u_image_scale: { value: imageScale },
+                    u_origin: { value: origin },
+                    u_image_dims: { value: imageDims },
+                    u_screen_dims: { value: screenDims }
+                }
+
+                var hexQuad = new THREE.Mesh(
+                    new THREE.PlaneGeometry(100.0, aspect * 100.0),
+                    new THREE.ShaderMaterial({
+                        vertexShader: MapShaders.buildMapVertexShader(),
+                        fragmentShader: MapShaders.buildMapFragmentShader(),
+                        depthWrite: false,
+                        depthTest: false,
+                        blending: THREE.AdditiveBlending,
+                        uniforms: mapUniforms
+                    })
+                );
+                hexQuad.position.set(0.0, 0.0, 0.001);
+                scene.add(hexQuad);
+
+                const controls = new OrbitControls(camera, renderer.domElement);
+
+                camera.position.set(0, 0, 5);
+                controls.update();
+
+                const gui = new GUI()
+                const mapFolder = gui.addFolder("Map Controls")
+                mapFolder.add(params, "p_image_scale").min(0.001).max(1.0).step(0.001).onChange(function () {
+                    mapUniforms.u_image_scale.value = params.p_image_scale;
+                    renderer.render(scene, camera);
+                });
+                mapFolder.add(params, "p_grid_scale").min(100).max(1000.0).step(0.001).onChange(function () {
+                    mapUniforms.u_grid_scale.value = params.p_grid_scale;
+                    renderer.render(scene, camera);
+                });
+                mapFolder.add(params, "p_origin_x").min(0.0).max(1.0).step(0.001).onChange(function () {
+                    mapUniforms.u_origin.value.x = params.p_origin_x;
+                    renderer.render(scene, camera);
+                });
+                mapFolder.add(params, "p_origin_y").min(0.0).max(1.0).step(0.001).onChange(function () {
+                    mapUniforms.u_origin.value.y = params.p_origin_y;
+                    renderer.render(scene, camera);
+                });
+
+                mapFolder.add(params, "p_grid_rot_x").min(-3.0).max(3.0).step(0.001).onChange(function () {
+                    hexQuad.rotation.set(params.p_grid_rot_x, params.p_grid_rot_y, params.p_grid_rot_z);
+                    renderer.render(scene, camera);
+                });
+                // mapFolder.add(params, "p_grid_rot_y").min(-1.0).max(1.0).step(0.001).onChange(function () {
+                //     hexQuad.rotation.set(params.p_grid_rot_x, params.p_grid_rot_y, params.p_grid_rot_z);
+                //     renderer.render(scene, camera);
+                // });
+                mapFolder.add(params, "p_grid_pos_y").min(-10.0).max(10.0).step(0.001).onChange(function () {
+                    hexQuad.position.setY(params.p_grid_pos_y);
+                    renderer.render(scene, camera);
+                });
+                mapFolder.add(params, "p_grid_rot_z").min(-3.0).max(3.0).step(0.001).onChange(function () {
+                    hexQuad.rotation.set(params.p_grid_rot_x, params.p_grid_rot_y, params.p_grid_rot_z);
+                    renderer.render(scene, camera);
+                });
+
+                mapFolder.open()
+
+                //container.appendChild(gui.domElement);
 
                 function animate() {
-                    
-                    requestAnimationFrame( animate );
+
+                    requestAnimationFrame(animate);
 
                     // cube.rotation.x += 0.005;
                     // cube.rotation.y += 0.01;
-            
-                    renderer.render( scene, camera );
-            
+
+                    controls.update();
+
+                    renderer.render(scene, camera);
+
                 }
 
                 animate();
-        
+
             },
 
             // onProgress callback currently not supported
             undefined,
 
             // onError callback
-            function ( err ) {
-                console.error( 'An error happened.' );
+            function (err) {
+                console.error('An error happened.');
             }
         );
 
- 
+
 
 
     }
--- src/shaders/map_frag.glsl
+++ src/shaders/map_frag.glsl
@@ -1,7 +1,132 @@
-  varying vec2 vUv;
+varying vec2 vUv;
 
-  uniform sampler2D baseMap;
+uniform float u_image_scale;
+uniform float u_grid_scale;
+uniform vec2 u_origin;
+uniform vec2 u_image_dims;
+uniform vec2 u_screen_dims;
 
-  void main() {
-    gl_FragColor = texture2D(baseMap, vUv);
-  }
+uniform sampler2D baseMap;
+
+#extension GL_OES_standard_derivatives : enable
+
+//#define FLAT_TOP_HEXAGON
+
+// Helper vector. If you're doing anything that involves regular triangles or hexagons, the
+// 30-60-90 triangle will be involved in some way, which has sides of 1, sqrt(3) and 2.
+#ifdef FLAT_TOP_HEXAGON
+  vec2 s = vec2(1.7320508, 1.0);
+#else
+  vec2 s = vec2(1.0, 1.7320508);
+#endif
+
+float hash21(vec2 p) {
+  return fract(sin(dot(p, vec2(141.13, 289.97))) * 43758.5453);
+}
+
+// The 2D hexagonal isosuface function: If you were to render a horizontal line and one that
+// slopes at 60 degrees, mirror, then combine them, you'd arrive at the following. As an aside,
+// the function is a bound -- as opposed to a Euclidean distance representation, but either
+// way, the result is hexagonal boundary lines.
+float hex(in vec2 p) {
+  p = abs(p);
+
+    #ifdef FLAT_TOP_HEXAGON
+  return max(dot(p, s * .5), p.y); // Hexagon.
+    #else
+  return max(dot(p, s * .5), p.x); // Hexagon.
+    #endif    
+}
+
+// This function returns the hexagonal grid coordinate for the grid cell, and the corresponding 
+// hexagon cell ID -- in the form of the central hexagonal point. That's basically all you need to 
+// produce a hexagonal grid.
+//
+// When working with 2D, I guess it's not that important to streamline this particular function.
+// However, if you need to raymarch a hexagonal grid, the number of operations tend to matter.
+// This one has minimal setup, one "floor" call, a couple of "dot" calls, a ternary operator, etc.
+// To use it to raymarch, you'd have to double up on everything -- in order to deal with 
+// overlapping fields from neighboring cells, so the fewer operations the better.
+vec4 getHex(vec2 p) {    
+    // The hexagon centers: Two sets of repeat hexagons are required to fill in the space, and
+    // the two sets are stored in a "vec4" in order to group some calculations together. The hexagon
+    // center we'll eventually use will depend upon which is closest to the current point. Since 
+    // the central hexagon point is unique, it doubles as the unique hexagon ID.
+
+    #ifdef FLAT_TOP_HEXAGON
+  vec4 hC = floor(vec4(p, p - vec2(1, .5)) / s.xyxy) + .5;
+    #else
+  vec4 hC = floor(vec4(p, p - vec2(.5, 1)) / s.xyxy) + .5;
+    #endif
+
+    // Centering the coordinates with the hexagon centers above.
+  vec4 h = vec4(p - hC.xy * s, p - (hC.zw + .5) * s);
+
+    // Nearest hexagon center (with respect to p) to the current point. In other words, when
+    // "h.xy" is zero, we're at the center. We're also returning the corresponding hexagon ID -
+    // in the form of the hexagonal central point.
+    //
+    // On a side note, I sometimes compare hex distances, but I noticed that Iomateron compared
+    // the squared Euclidian version, which seems neater, so I've adopted that.
+  return dot(h.xy, h.xy) < dot(h.zw, h.zw) ? vec4(h.xy, hC.xy) : vec4(h.zw, hC.zw + .5);
+}
+
+void main() {
+
+// Aspect correct screen coordinates.
+  float screen_aspect = u_image_dims.y / u_image_dims.x;
+  vec2 u = vec2(vUv.x, screen_aspect * vUv.y);
+
+    // Scaling, translating, then converting it to a hexagonal grid cell coordinate and
+    // a unique coordinate ID. The resultant vector contains everything you need to produce a
+    // pretty pattern, so what you do from here is up to you.
+  vec4 h = getHex(u_grid_scale * u + s.yx / 2.);
+
+    // The beauty of working with hexagonal centers is that the relative edge distance will simply 
+    // be the value of the 2D isofield for a hexagon.
+  float eDist = hex(h.xy); // Edge distance.
+
+  float u_contour_spacing = u_image_scale * 2.0;
+    float u_contour_width = 0.15;
+
+    const vec4 col_contour = vec4(0.9, 0.9, 0.9, 0.9);
+    const vec4 col_outside = vec4(0.1, 0.1, 0.1, 1.0);
+    const vec4 col_inside = vec4(0.6, 0.6, 0.6, 1.0);
+
+    vec4 col = mix(col_inside, col_outside, step(0.0, eDist));
+    float dist_change = fwidth(eDist) * 0.5;
+
+    // Major contour lines
+    {
+        float spacing = u_contour_spacing;
+        float width = u_contour_width;
+
+        float t = abs(fract(eDist / spacing + 0.5) - 0.5) * spacing;
+        t = smoothstep(width - dist_change, width + dist_change, t);
+        col = mix(col_contour, col, t);
+    }
+
+    // Minor contour lines
+    {
+        float spacing = u_contour_spacing * 0.25;
+        float width = u_contour_width * 0.25;
+
+        float t = abs(fract(eDist / spacing + 0.5) - 0.5) * spacing;
+        t = smoothstep(width - dist_change, width + dist_change, t);
+        col = mix(col_contour, col, t);
+    }
+
+    // Initiate the background to a white color, putting in some dark borders.
+  // vec4 hexCol = mix(vec4(1., 1., 1., 0.), vec4(0., 0., 0., 1.), smoothstep(0., 0.022, eDist - .5 + .04)); 
+
+    // float inv_scale = 1.0 / u_image_scale;
+    // float offset = 0.5 * (1.0 - inv_scale);
+
+    // vec2 uv_remap = inv_scale * vUv + vec2(offset) + (u_origin - vec2(0.5));
+
+    // vec4 mapCol = texture2D(baseMap, uv_remap );
+    
+
+  gl_FragColor = u_origin.x * col;//(vec4(hexCol, 1.0) + vec4(0.3)) * mapCol;//mix(vec4(hexCol, 0.8), mapCol, 0.8);
+
+}
